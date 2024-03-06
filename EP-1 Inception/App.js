8d232ef30bb63f758a0b2500414d235fd0e29058
shirshak <84394640+shirshakmaurya@users.noreply.github.com>
2024-03-07 04:55:42 +0530

    created App.js

---
+++ EP-1 Inception/App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("div", {id:"parent"}, 
+    [React.createElement("h1", {id: "heading"},"Hello React from div"),
+     React.createElement("h2",{id:"secondheading"},"Heading h2")
+    ])
+    ;
+    
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
