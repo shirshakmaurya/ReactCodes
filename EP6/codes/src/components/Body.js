4b20b1c4fbd2316453f403488f3a2fdcdd67fe27
shirshak <84394640+shirshakmaurya@users.noreply.github.com>
2024-03-11 15:28:27 +0530

    using useEffect hook and live api

---
+++ EP6/codes/src/components/Body.js
@@ -0,0 +1,54 @@
+import RestaurantCard from "./RestaurantCard";
+import { useState, useEffect } from "react";
+
+const Body = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.45970&lng=77.02820&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    // console.log(json);
+    // console.log(json?.data?.cards[4]);
+    console.log(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setListOfRestaurant(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log(listOfRestaurant);
+            const filteredList = listOfRestaurant.filter(
+              (res) => res.info.avgRating > 4.2
+            );
+            setListOfRestaurant(filteredList);
+            console.log(listOfRestaurant, "clicked");
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurant.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
