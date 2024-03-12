d343c10d02cf14400aa433757905516cbb318dad
shirshak <84394640+shirshakmaurya@users.noreply.github.com>
2024-03-12 15:42:57 +0530

    login/logout btn useState

--- EP6/codes/src/components/Header.js
+++ EP6/codes/src/components/Header.js
@@ -1,22 +1,33 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="logo-container">
-                <img className="logo" src={LOGO_URL} />
-                
-            </div>
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+  const [btnName, setBtnName] = useState("Login");
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+          <li>
+            <button
+              onClick={() => {
+                setBtnName(btnName === "Login" ? "Logout" : "Login");
+              }}
+            >
+              {btnName}
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
