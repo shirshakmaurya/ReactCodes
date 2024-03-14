11d35af455732a6bee81f4f69d1cde1c4923be95
shirshak <84394640+shirshakmaurya@users.noreply.github.com>
2024-03-14 16:53:05 +0530

    created Error component and used useRouteError

--- EP7/Codes/src/App.js
+++ EP7/Codes/src/App.js
@@ -5,6 +5,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import About from "./components/About";
 import Body from "../src/components/Body";
 import Header from "./components/Header";
+import Error from "./components/Error";
+import Contact from "./components/Contact";
 
 const AppLayout = () => {
   return (
@@ -16,8 +18,9 @@ const AppLayout = () => {
 };
 
 const appRouter = createBrowserRouter([
-  { path: "/", element: <AppLayout /> },
+  { path: "/", element: <AppLayout />, errorElement: <Error /> },
   { path: "/about", element: <About /> },
+  { path: "/contact", element: <Contact /> },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
