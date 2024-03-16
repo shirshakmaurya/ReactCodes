b49f82671284b698222ab2da62091f368831bba4
shirshak <84394640+shirshakmaurya@users.noreply.github.com>
2024-03-17 04:52:12 +0530

    states in class based component

--- EP8/Codes/User.js
+++ EP8/Codes/User.js
@@ -3,10 +3,20 @@ import React from "react";
 class User extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = { count: 0, count2: 2 };
   }
   render() {
     return (
       <div>
+        <h1>Count: {this.state.count}</h1>
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+        >
+          Increase Count
+        </button>
         <h1>Name:{this.props.name}</h1>
         <h1>Contact:{"@Tony"}</h1>
       </div>
