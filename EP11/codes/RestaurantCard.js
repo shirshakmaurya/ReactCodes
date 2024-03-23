5a3eccf842d7c8075f9ff4c3b1279d824a98442b
shirshak <84394640+shirshakmaurya@users.noreply.github.com>
2024-03-23 20:14:19 +0530

    Higher Order Component

---
+++ EP11/codes/RestaurantCard.js
@@ -0,0 +1,33 @@
+import { CDN_URL } from "../utils/constants";
+
+const RestaurantCard = (props) => {
+  // console.log(props)
+
+  const { name, cuisines, avgRating, sla, cloudinaryImageId } =
+    props.resData.info;
+  return (
+    <div className="p-4 m-4 w-52 h-96 rounded-lg  bg-gray-100  hover:bg-gray-300">
+      <img className="w-44 h-36 rounded-lg" src={CDN_URL + cloudinaryImageId} />
+
+      <h3 className="font-bold">{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating}</h4>
+      <h4>{sla.deliveryTime} Minutes</h4>
+    </div>
+  );
+};
+
+export const withPromotedLabel = (RestaurantCard) => {
+  return (props) => {
+    const { resData } = props;
+    return (
+      <div>
+        <label className="absolute bg-black text-white ml-3">High Rated</label>
+        <RestaurantCard {...props} />
+        {/* <RestaurantCard resData={resData} /> */}
+      </div>
+    );
+  };
+};
+
+export default RestaurantCard;
